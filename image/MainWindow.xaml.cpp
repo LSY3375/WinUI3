b7ba1ef833ae12dc21e9dcad3f0c5b862c1f96c8
LSY <105796523+LSY3375@users.noreply.github.com>
2022-12-01 13:29:31 +0900

    Add files via upload

---
+++ image/MainWindow.xaml.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+#include "MainWindow.xaml.h"
+#if __has_include("MainWindow.g.cpp")
+#include "MainWindow.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winrt::Final2::implementation
+{
+    MainWindow::MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    int32_t MainWindow::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainWindow::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+
+}
---
+++ image/MainWindow.xaml.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "MainWindow.g.h"
+
+namespace winrt::Final2::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+    };
+}
+
+namespace winrt::Final2::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
