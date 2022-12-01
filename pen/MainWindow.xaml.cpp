fab18ffb7e56b3450ec33ad61773b98d60704a44
LSY <105796523+LSY3375@users.noreply.github.com>
2022-12-01 13:30:07 +0900

    Add files via upload

---
+++ pen/MainWindow.xaml.cpp
@@ -0,0 +1,167 @@
+﻿#include "pch.h"
+#include <microsoft.ui.xaml.window.h>
+#include "MainWindow.xaml.h"
+#if __has_include("MainWindow.g.cpp")
+#include "MainWindow.g.cpp"
+#include <winrt/Windows.Storage.h>
+#include <winrt/Windows.Storage.Streams.h>
+#include <winrt/Windows.Storage.Pickers.h>
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.UI.Xaml.Media.Imaging.h>
+#include "winrt/Windows.Media.Ocr.h"
+#endif
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+using namespace Windows::Storage;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winrt::Final3::implementation
+{
+    MainWindow::MainWindow()
+    {
+        InitializeComponent();
+        auto windowNative{ this->try_as<::IWindowNative>() };
+        HWND hWnd{ 0 };
+        windowNative->get_WindowHandle(&hWnd);
+        SetWindowPos(hWnd, NULL, 800, 200, 820, 590, NULL);
+        flag = false;
+        px = 100;
+        py = 100;
+        mySize = 16;
+    }
+
+    int32_t MainWindow::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainWindow::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+
+}
+struct winrt::Windows::UI::Color myCol = winrt::Microsoft::UI::Colors::Green();
+void winrt::Final3::implementation::MainWindow::CanvasControl_Draw(winrt::Microsoft::Graphics::Canvas::UI::Xaml::CanvasControl const& sender, winrt::Microsoft::Graphics::Canvas::UI::Xaml::CanvasDrawEventArgs const& args)
+{
+    CanvasControl canvas = sender.as<CanvasControl>();
+    int n = vx.size();
+    for (int i = 1; i < n; i++) {
+        if (vx[i] == 0 && vy[i] == 0) {
+            i++; continue;
+        }
+        args.DrawingSession().DrawLine(vx[i - 1], vy[i - 1], vx[i], vy[i], col[i], size[i]);
+        args.DrawingSession().FillCircle(vx[i - 1], vy[i - 1], size[i] / 2, col[i]);
+        args.DrawingSession().FillCircle(vx[i], vy[i], size[i] / 2, col[i]);
+    }
+
+    //args.DrawingSession().DrawEllipse(px, py, 80, 30, Colors::Black(), 8);
+    //args.DrawingSession().DrawText(L"Hello, world!", px-55, py-15, Colors::Yellow());
+}
+
+void winrt::Final3::implementation::MainWindow::CanvasControl_PointerPressed(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Input::PointerRoutedEventArgs const& e)
+{
+    flag = true;
+}
+void winrt::Final3::implementation::MainWindow::CanvasControl_PointerMoved(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Input::PointerRoutedEventArgs const& e)
+{
+    CanvasControl canvas = sender.as<CanvasControl>();
+    px = e.GetCurrentPoint(canvas).Position().X;
+    py = e.GetCurrentPoint(canvas).Position().Y;
+    if (flag) {
+        vx.push_back(px);
+        vy.push_back(py);
+        col.push_back(myCol);
+        size.push_back(mySize);
+        canvas.Invalidate();
+    }
+}
+void winrt::Final3::implementation::MainWindow::ColorPicker_ColorChanged(winrt::Microsoft::UI::Xaml::Controls::ColorPicker const& sender, winrt::Microsoft::UI::Xaml::Controls::ColorChangedEventArgs const& args)
+{
+    myCol = args.NewColor();
+}
+
+void winrt::Final3::implementation::MainWindow::CanvasControl_PointerReleased(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Input::PointerRoutedEventArgs const& e)
+{
+    flag = false;
+    px = py = 0.0;
+    vx.push_back(px);
+    vy.push_back(py);
+    col.push_back(myCol);
+    size.push_back(mySize);
+}
+
+void winrt::Final3::implementation::MainWindow::CanvasControl_PointerEntered(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Input::PointerRoutedEventArgs const& e)
+{
+    CanvasControl canvas = sender.as<CanvasControl>();
+    canvas.Invalidate();
+}
+
+void winrt::Final3::implementation::MainWindow::Slider_ValueChanged(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Controls::Primitives::RangeBaseValueChangedEventArgs const& e)
+{
+    mySize = e.NewValue();
+}
+
+void winrt::Final3::implementation::MainWindow::Savebtn_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
+{
+    FILE* fw;
+    errno_t err;
+    err = fopen_s(&fw, "C:/Users/1/Desktop/my.txt", "w");// +, ccs = UTF - 8");
+    if (err == 0)
+    {
+        MessageBox(NULL, L"The file was opened\n", L"hi1", NULL);
+        int num = vx.size();
+        fprintf_s(fw, "%d\n", num);
+        for (int i = 0; i < num; i++) {
+            fprintf_s(fw, "%f %f %hhu %hhu %hhu %hhu %f\n", vx[i], vy[i],
+                col[i].A, col[i].B, col[i].G, col[i].R, size[i]);
+        }
+        fclose(fw);
+        vx.clear();
+        vy.clear();
+        col.clear();
+        size.clear();
+    }
+    else  MessageBox(NULL, L"The file was not opened\n", L"hi2", NULL);
+}
+
+
+void winrt::Final3::implementation::MainWindow::Loadbtn_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
+{
+    FILE* fr;
+    errno_t err;
+    err = fopen_s(&fr, "C:/Users/1/Desktop/my.txt", "r");// +, ccs = UTF - 8");
+    if (err == 0)
+    {
+        MessageBox(NULL, L"The file was opened\n", L"hi1", NULL);
+        vx.clear(); vy.clear(); size.clear(); col.clear();
+        int num;
+        float vx1, vy1, size1;
+        unsigned char colA, colB, colG, colR;
+
+        fscanf_s(fr, "%d", &num);
+        for (int i = 0; i < num; i++) {
+            fscanf_s(fr, "%f %f %hhu %hhu %hhu %hhu %f ", &vx1, &vy1, &colA, &colB, &colG, &colR, &size1);
+            vx.push_back(vx1);
+            vy.push_back(vy1);
+            myCol.A = colA;
+            myCol.B = colB;
+            myCol.G = colG;
+            myCol.R = colR;
+            col.push_back(myCol);
+            size.push_back(size1);
+        }
+        fclose(fr);
+        MessageBox(NULL, L"The file closed\n", L"hi1", NULL);
+        CanvasControl_PointerReleased(NULL, NULL);
+    }
+    else  MessageBox(NULL, L"The file was not opened\n", L"hi2", NULL);
+}
+
+
+
+
---
+++ pen/MainWindow.xaml.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+
+#include "MainWindow.g.h"
+#include <winrt/Microsoft.Graphics.Canvas.UI.Xaml.h> // for canvas
+#include <winrt/Microsoft.UI.Xaml.Input.h>
+#include <winrt/Microsoft.UI.Input.h>
+#include <winrt/Windows.Storage.h>
+#include <winrt/Windows.Storage.Streams.h>
+#include <winrt/Windows.Storage.Pickers.h>
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.UI.Xaml.Media.Imaging.h>
+
+using namespace winrt::Microsoft::UI;
+using namespace winrt::Microsoft::Graphics::Canvas::UI::Xaml;
+
+namespace winrt::Final3::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow();
+
+        int32_t MyProperty();
+        bool flag;
+        float px, py;
+        float mySize;
+        std::vector<float> vx;
+        std::vector<float> vy;
+        std::vector<struct winrt::Windows::UI::Color> col;
+        std::vector<float> size;
+        void MyProperty(int32_t value);
+        void CanvasControl_Draw(winrt::Microsoft::Graphics::Canvas::UI::Xaml::CanvasControl const& sender, winrt::Microsoft::Graphics::Canvas::UI::Xaml::CanvasDrawEventArgs const& args);
+        void CanvasControl_PointerPressed(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Input::PointerRoutedEventArgs const& e);
+        void CanvasControl_PointerMoved(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Input::PointerRoutedEventArgs const& e);
+        void ColorPicker_ColorChanged(winrt::Microsoft::UI::Xaml::Controls::ColorPicker const& sender, winrt::Microsoft::UI::Xaml::Controls::ColorChangedEventArgs const& args);
+        void CanvasControl_PointerReleased(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Input::PointerRoutedEventArgs const& e);
+        void Slider_ValueChanged(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Controls::Primitives::RangeBaseValueChangedEventArgs const& e);
+        void Savebtn_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
+        void Loadbtn_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
+        void CanvasControl_PointerEntered(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Input::PointerRoutedEventArgs const& e);
+    };
+}
+
+namespace winrt::Final3::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
