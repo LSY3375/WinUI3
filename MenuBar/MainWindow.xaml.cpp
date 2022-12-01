2a42746fe0b04e3337859ee34a0bd0c29132d95d
LSY <105796523+LSY3375@users.noreply.github.com>
2022-12-01 13:37:25 +0900

    Add files via upload

---
+++ MenuBar/MainWindow.xaml.cpp
@@ -0,0 +1,38 @@
+﻿#include "pch.h"
+#include "MainWindow.xaml.h"
+#if __has_include("MainWindow.g.cpp")
+#include "MainWindow.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winrt::App1::implementation
+{
+    MainWindow::MainWindow()
+    {
+        InitializeComponent();
+        this->Title(L"I love WinUI3");
+    }
+
+    int32_t MainWindow::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainWindow::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+}
+
+void winrt::App1::implementation::MainWindow::MenuFlyoutItem_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
+{
+    TBlock().Text(L"You Clicked : I love WinUI3");
+    MessageBox(NULL, L"I love WinUI3", L"도움말", NULL);
+
+}
---
+++ MenuBar/MainWindow.xaml.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "MainWindow.g.h"
+
+namespace winrt::App1::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+        void MenuFlyoutItem_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
+    };
+}
+
+namespace winrt::App1::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
